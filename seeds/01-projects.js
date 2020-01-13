2d4b61ffe614d10140b77e437d6c8fc96ed718f8
smv5047 <smv5047@gmail.com>
2020-01-12 20:56:50 -0500

    completed seed and laid out structure for routers and endpoint

--- seeds/01-projects.js
+++ seeds/01-projects.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
-        {project_name: "testproject1", project_description:"This is a test project"},
-        {project_name: "testproject2", project_description:"This is also a test project"},
+        {project_name: "testproject1", project_description:"This is a test project", is_complete: false},
+        {project_name: "testproject2", project_description:"This is also a test project", is_complete: false},
         {project_name: "testproject3", project_description:"This too is a test project", is_complete: true}
 
       ]);
--- seeds/03-tasks.js
+++ seeds/03-tasks.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('tasks').del()
+  return knex('tasks').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('tasks').insert([
-        {task_description: "testtask1", task_note: "testnote1", project_id: 1},
-        {task_description: "testtask1", task_note: "testnote2", project_id: 2},
+        {task_description: "testtask1", task_note: "testnote1", is_complete: false, project_id: 1},
+        {task_description: "testtask1", task_note: "testnote2", is_complete: false, project_id: 2},
         {task_description: "testtask1", task_note: "testnote3", is_complete: true, project_id: 3}
       ]);
     });
